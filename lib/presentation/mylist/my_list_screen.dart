65940309890624e93bf31f6f9a0c4057c83d6bee
ben10show1999 <ben10show1999@gmail.com>
2026-01-29 05:27:46 +0000

    my_video_project

---
+++ lib/presentation/mylist/my_list_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../../core/components/universal_image.dart';
+import '../../core/theme/app_colors.dart';
+import '../details/movie_details_screen.dart';
+class MyListScreen extends StatelessWidget { const MyListScreen({super.key}); 
+  @override Widget build(BuildContext context) { 
+    final l = List.generate(6, (i) => "https://via.placeholder.com/300x450?text=Saved+$i"); 
+    return Scaffold(backgroundColor: AppColors.background, appBar: AppBar(title: const Text("My List")), body: Padding(padding: const EdgeInsets.all(16), child: LayoutBuilder(builder: (c, cs) { 
+      int cnt = (cs.maxWidth / 140).floor().clamp(3, 8); 
+      return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: cnt, childAspectRatio: 0.55, crossAxisSpacing: 16, mainAxisSpacing: 16), itemCount: l.length, itemBuilder: (c, i) => InkWell(onTap: () => Navigator.push(c, MaterialPageRoute(builder: (_) => const MovieDetailsScreen())), child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [ClipRRect(borderRadius: BorderRadius.circular(8), child: UniversalImage(path: l[i])), const SizedBox(height: 8), Text("Movie Title $i", maxLines: 2, overflow: TextOverflow.ellipsis, style: const TextStyle(color: Colors.white, fontSize: 13))]))); 
+    }))); 
+  } 
+}
