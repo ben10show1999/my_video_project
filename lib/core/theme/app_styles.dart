a594e16ae86d42fe8b8ce17e533eb78b20e8cfcf
ben10show1999 <ben10show1999@gmail.com>
2026-02-08 08:28:32 +0000

    my_video_project

--- lib/core/theme/app_styles.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'app_colors.dart';
-class AppStyles { 
-  static const TextStyle headlineLarge = TextStyle(color: AppColors.textPrimary, fontSize: 26, fontWeight: FontWeight.bold, height: 1.2); 
-  static const TextStyle headlineMedium = TextStyle(color: AppColors.textPrimary, fontSize: 20, fontWeight: FontWeight.bold); 
-  static const TextStyle bodyText = TextStyle(color: AppColors.textSecondary, fontSize: 14, height: 1.5);
-  static const TextStyle tagText = TextStyle(color: AppColors.textPrimary, fontSize: 11, fontWeight: FontWeight.w600); 
-}
